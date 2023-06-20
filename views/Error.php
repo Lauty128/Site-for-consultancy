4b9be7a6cfd84310ca463353676c9ffeeb547061
Lauty128 <lautarosilverii@gmail.com>
2023-06-20 10:44:44 -0300

    views created

---
+++ views/Error.php
@@ -0,0 +1,90 @@
+<!doctype html>
+<html>
+
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+    <title>Error 404 - Page Not Found!</title>
+</head>
+
+<body>
+    <div class="container">
+        <img class="ops" src="public/assets/404.svg" />
+        <br />
+        <h3>Esta pagina no existe.
+            <br /> Vuelve al inicio de la pagina para seguir navegando.</h3>
+        <br />
+        <?php
+            echo "<a class='buton' href='/'>INICIO</a>"
+        ?>
+    </div>
+</body>
+
+<style>
+
+body {
+    background: #112D4E;
+    padding: 0;
+    margin: 0;
+    font-family: Helvetica, Arial, sans-serif;
+}
+
+.container {
+    background-color: #112D4E;
+    margin: 0 auto;
+    text-align: center;
+    padding-top: 50px;
+}
+
+h3 {
+    font-size: 16px;
+    color: #3498db;
+    font-weight: bold;
+    text-align: center;
+    line-height: 130%;
+}
+
+.buton {
+    background: #3498db;
+    padding: 10px 20px;
+    color: #fff;
+    font-weight: bold;
+    text-align: center;
+    border-radius: 3px;
+    text-decoration: none;
+}
+
+a:hover {
+    color: #112D4E;
+}
+
+span {
+    font-size: 14px;
+    color: #FFF;
+    font-weight: normal;
+    text-align: center;
+}
+
+span a {
+    color: #FF0;
+    text-decoration: none;
+}
+
+span a:hover {
+    color: #F00;
+}
+
+@media screen and (max-width: 500px) {
+    img {
+        width: 70%;
+    }
+    .container {
+        padding: 70px 10px 10px 10px;
+    }
+    h3 {
+        font-size: 14px;
+    }
+}
+</style>
+
+</html>
---
+++ views/templates/header.php
@@ -0,0 +1,53 @@
+<div class="Top-information">
+    <img src="public/assets/notfound.jpg" alt="logo empresa" class="Top-information__image">
+    <div class="Top-information__div">
+        <span class="Top-information__span">
+            <svg width="24px" height="24px" stroke-width="1.5" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" color="#F9F7F7"><path d="M18.118 14.702L14 15.5c-2.782-1.396-4.5-3-5.5-5.5l.77-4.13L7.815 2H4.064c-1.128 0-2.016.932-1.847 2.047.42 2.783 1.66 7.83 5.283 11.453 3.805 3.805 9.286 5.456 12.302 6.113 1.165.253 2.198-.655 2.198-1.848v-3.584l-3.882-1.479z" stroke="#F9F7F7" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"></path></svg>
+            2284 - 000000
+        </span>
+        <span class="Top-information__span">
+            <svg width="24px" height="24px" stroke-width="1.5" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" color="#F9F7F7"><path d="M7 9l5 3.5L17 9" stroke="#F9F7F7" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"></path><path d="M2 17V7a2 2 0 012-2h16a2 2 0 012 2v10a2 2 0 01-2 2H4a2 2 0 01-2-2z" stroke="#F9F7F7" stroke-width="1.5"></path></svg>
+            consultas@dominio.com
+        </span>
+    </div>
+</div>
+
+
+<style>
+
+    body{
+        padding: 0;
+        margin: 0;
+        width: 100%;
+        min-height: 100%;
+    }
+
+    .Top-information{
+        background-color: #112D4E;
+        position: relative;
+        padding: .3em .8em;
+    }
+    .Top-information__image{
+        width: 90px;
+        height: 50px;
+        object-fit: cover;
+    }
+    .Top-information__div{
+        float: right;
+        width: 70%;
+        height: 50px;
+        display: flex;
+        align-items: center;
+        justify-content: flex-end;
+        gap: 1.5em;
+    }
+    .Top-information__span{
+        display: flex;
+        float: right;
+        width: fit-content;
+        align-items: center;
+        gap: .2em;
+        color: #F9F7F7;
+        font-weight: 800;
+    }
+</style>

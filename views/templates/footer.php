2d74e4e6b68b61977d5f6d3b0c2c8c17a6dbb5fd
Lauty128 <lautarosilverii@gmail.com>
2023-06-29 11:17:41 -0300

    logo added and footer updated with effects

--- views/templates/footer.php
+++ views/templates/footer.php
@@ -4,22 +4,39 @@
 
 <footer class="Footer">
     <div class="Footer__mapsContainer">
+        <img src="public/assets/logo.png" alt="logo empresa" class="Footer__image">
         <div class="Footer__map">
             <span class="Footer__span">Servicios</span>
-            <a href="#" class="Footer__a">Servicio 1</a>
-            <a href="#" class="Footer__a">Servicio 2</a>
-            <a href="#" class="Footer__a">Servicio 3</a>
-            <a href="#" class="Footer__a">Servicio 4</a>
+            <a href="#" class="Footer__a">
+                <svg width="15px" height="15px" stroke-width="1.7" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" color="#FFFF"><path d="M9 6l6 6-6 6" stroke="#FFFF" stroke-width="1.7" stroke-linecap="round" stroke-linejoin="round"></path></svg>
+                Sistema de gestion
+            </a>
+            <a href="#" class="Footer__a">
+            <svg width="15px" height="15px" stroke-width="1.7" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" color="#FFFF"><path d="M9 6l6 6-6 6" stroke="#FFFF" stroke-width="1.7" stroke-linecap="round" stroke-linejoin="round"></path></svg>
+                RRHH
+            </a>
+            <a href="#" class="Footer__a">
+                <svg width="15px" height="15px" stroke-width="1.7" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" color="#FFFF"><path d="M9 6l6 6-6 6" stroke="#FFFF" stroke-width="1.7" stroke-linecap="round" stroke-linejoin="round"></path></svg>    
+                Programa de alto rendimiento
+            </a>
+            <a href="#" class="Footer__a">
+                <svg width="15px" height="15px" stroke-width="1.7" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" color="#FFFF"><path d="M9 6l6 6-6 6" stroke="#FFFF" stroke-width="1.7" stroke-linecap="round" stroke-linejoin="round"></path></svg>    
+                Proyectos de mejora
+            </a>
+            <a href="#" class="Footer__a">
+                <svg width="15px" height="15px" stroke-width="1.7" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" color="#FFFF"><path d="M9 6l6 6-6 6" stroke="#FFFF" stroke-width="1.7" stroke-linecap="round" stroke-linejoin="round"></path></svg>    
+                Nube
+            </a>
         </div>
         <div class="Footer__map">
             <span class="Footer__span">Mapa del sitio</span>
             <a href="/" class="Footer__a">Inicio</a>
             <a href="/quienes-somos" class="Footer__a">Quienes somos</a>
             <a href="/servicios/" class="Footer__a">Servicios</a>
-            <a href="#" class="Footer__a">Cursos</a>
-            <a href="#" class="Footer__a">Vacantes</a>
-            <a href="#" class="Footer__a">Novedades</a>
-            <a href="#" class="Footer__a">Contacto</a>
+            <a href="/cursos" class="Footer__a">Cursos</a>
+            <a href="/vacantes" class="Footer__a">Vacantes</a>
+            <a href="/novedades" class="Footer__a">Novedades</a>
+            <a href="/contacto" class="Footer__a">Contacto</a>
         </div>
     </div>
 

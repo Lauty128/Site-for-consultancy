56093f7c0ae54b7278ddcfbd7b31629d25606e01
Lauty128 <lautarosilverii@gmail.com>
2023-07-08 12:29:29 -0300

    little change in vacancies page & reading of a vacancy added on vacancy page

--- views/vacante.php
+++ views/vacante.php
@@ -1,17 +1,68 @@
+<?php 
+
+    require '../config/dataBase.php';
+    //echo $_GET['id'];
+    $db_class = new Database();
+    $PDO = $db_class->connect_to_database();
+
+    if(get_class($PDO) == 'PDOException'){ 
+        header('Location: /Error'); 
+        die();
+    }
+    else{
+        $query = "SELECT * FROM vacancy WHERE id_vacancy = '".$_GET['id']."'";
+        //echo $query;
+        $call = $PDO->prepare($query);
+        $call->execute();
+        
+        $vacancy = $call->fetchAll(PDO::FETCH_ASSOC);
+    }
+
+?>
+
 <!DOCTYPE html>
-<html lang="en">
+<html lang="es">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="/public/css/index.css">
+    <link rel="shortcut icon" href="/public/assets/logo.png" type="image/png">
     <title><?php echo $_GET['id']; ?></title>
 </head>
 <body>
+    <?php include_once "./templates/header.php" ?>
 
-    <pre>
-        <?php var_dump($_GET['id']); ?>
-    </pre>
-    <h2>En esta seccion se veran a fondo las vacantes</h2>
+    <header class="Header" style="height: fit-content; overflow:visible; background: none">
+        <?php include_once "./templates/menu.php" ?>
+    </header>
 
+    <!-- <pre>
+        <?php var_dump($vacancy); ?>
+    </pre> -->
+    <?php if(count($vacancy) == 1){ ?>
+        <h2> <?php echo $vacancy[0]["role"] ?> </h2>
+        <p><?php echo $vacancy[0]['description'] ?></p>
+        <div class="">
+            <h4>Funciones</h4>
+            <ul>
+                <?php foreach (explode("\n", $vacancy[0]['functions']) as $key => $function) {
+                    echo "<li>".$function."</li>";
+                } ?>
+            </ul>
+        </div>
+        <div class="">
+            <h4>Requisitos</h4>
+            <ul>
+                <?php foreach (explode("\n", $vacancy[0]['requirements']) as $key => $requirement) {
+                    echo "<li>".$requirement."</li>";
+                } ?>
+            </ul>
+        </div>
+    <?php } else{  ?>
+        <h2>NO EXISTE ESTE ARTICULO PAI</h2>
+    <?php } ?>
+    
+
+    <script src="/public/js/index.js"></script>
 </body>
 </html>
--- views/vacantes.php
+++ views/vacantes.php
@@ -20,7 +20,7 @@
 ?>
 
 <!DOCTYPE html>
-<html lang="en">
+<html lang="es">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -87,10 +87,6 @@
             <div class="ContactSection__card">
                 <svg width="26px" height="26px" stroke-width="1.7" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" color="#FFFF"><path d="M18.118 14.702L14 15.5c-2.782-1.396-4.5-3-5.5-5.5l.77-4.13L7.815 2H4.064c-1.128 0-2.016.932-1.847 2.047.42 2.783 1.66 7.83 5.283 11.453 3.805 3.805 9.286 5.456 12.302 6.113 1.165.253 2.198-.655 2.198-1.848v-3.584l-3.882-1.479z" stroke="#FFFF" stroke-width="1.7" stroke-linecap="round" stroke-linejoin="round"></path></svg>
                 2284 - 000000
-            </div>
-            <div class="ContactSection__card">
-            <svg width="26px" height="26px" stroke-width="1.5" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" color="#FFFF"><path d="M20 10c0 4.418-8 12-8 12s-8-7.582-8-12a8 8 0 1116 0z" stroke="#FFFF" stroke-width="1.5"></path><path d="M12 11a1 1 0 100-2 1 1 0 000 2z" fill="#FFFF" stroke="#FFFF" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"></path></svg>
-                Olavarria, Buenos Aires
             </div>
         </div>
         <form class="ContactSection__form">

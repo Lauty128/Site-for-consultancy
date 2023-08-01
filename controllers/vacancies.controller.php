014001591fe8ee59c5d389994f2dedabd1a25319
Lauty128 <lautarosilverii@gmail.com>
2023-08-01 19:08:49 -0300

    parameters added in vacancies controller

--- controllers/vacancies.controller.php
+++ controllers/vacancies.controller.php
@@ -4,12 +4,12 @@
 
     class VacanciesController{
 
-        static function getAll(int $page=0){
+        static function getAll(int $page=0, int $limit = 6){
             $db_class = new Database();
             $PDO = $db_class->connect_to_database(); 
             if(get_class($PDO) == 'PDOException'){ return null; }
 
-            $query = $PDO->prepare("SELECT id_vacancy,role,company,description,ubication,created_at FROM vacancy WHERE state = 1 ORDER BY created_at asc LIMIT 6 OFFSET ".($page * 6));
+            $query = $PDO->prepare("SELECT id_vacancy,role,company,description,ubication,created_at FROM vacancy WHERE state = 1 ORDER BY created_at asc LIMIT ".$limit." OFFSET ".($page * $limit));
             $query->execute();
             $vacancies = $query->fetchAll(PDO::FETCH_ASSOC);
 
@@ -24,7 +24,7 @@
             ];
         }
 
-        static function getOne(string $id){
+        static function getOne(string $id, string $attributes = '*'){
             $db_class = new Database();
             $PDO = $db_class->connect_to_database(); 
             if(get_class($PDO) == 'PDOException'){
@@ -32,7 +32,7 @@
                 die();
             }
 
-            $query = $PDO->prepare("SELECT * FROM vacancy WHERE id_vacancy = '$id'");
+            $query = $PDO->prepare("SELECT ".$attributes." FROM vacancy WHERE id_vacancy = '$id'");
             $query->execute();
             
             $vacancy = $query->fetchAll(PDO::FETCH_ASSOC);

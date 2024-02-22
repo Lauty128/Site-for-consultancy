2103f9c5f1e9a9c7b658aaadae078a0627eb37a7
Lauty128 <lautarosilverii@gmail.com>
2024-02-22 12:07:18 -0300

    Use minify and original css file on the same folder

--- views/templates/head.php
+++ views/templates/head.php
@@ -1,4 +1,4 @@
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
-<link rel="stylesheet" href="/public/css/index.css">
+<link rel="stylesheet" href="/public/css/index.min.css">
 <link rel="shortcut icon" href="/public/assets/statics/Logo.png" type="image/png">

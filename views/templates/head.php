d0de7d5b0a5008228d3b82d518c24bd81a24ef7f
Lauty128 <lautarosilverii@gmail.com>
2024-03-16 18:50:11 -0300

    Some keywords in head added

--- views/templates/head.php
+++ views/templates/head.php
@@ -1,5 +1,5 @@
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
-<meta name="keywords" content="soluciones eficientes, consultora industrial, consultoría industrial, empresas, Argentina, manufactura, producción, logística, cadena de suministro, maquinaria industrial, ingeniería, soluciones industriales, optimización de procesos, servicios industriales, asesoramiento empresarial">
+<meta name="keywords" content="soluciones eficientes, consultora industrial, empresa, empresarial, consultoría industrial, empresas, Argentina, manufactura, producción, logística, cadena de suministro, maquinaria industrial, ingeniería, soluciones industriales, optimización de procesos, servicios industriales, asesoramiento, asesoramiento empresarial, pymes, rrhh, recursos humanos">
 <link rel="stylesheet" href="/public/css/index.min.css">
 <link rel="shortcut icon" href="/public/assets/statics/Logo.png" type="image/png">
